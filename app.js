8849892a456f940e244f1a79d3c995298ef2035b
Jakob Heintze <jheintze33@gmail.com>
2023-08-08 13:50:34 +0200

    add gitignore

--- app.js
+++ app.js
@@ -11,9 +11,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 // mongoose.connect("mongodb://127.0.0.1:27017/todolistDB");
-mongoose.connect(
-  "mongodb+srv://Jheintze:99mongo99@cluster0.wqkkxwr.mongodb.net/todolistDB"
-);
+mongoose.connect(process.env.MONGO_URL);
 
 const itemsSchema = {
   name: String,
